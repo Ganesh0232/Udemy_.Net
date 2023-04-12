4d58ada30f46ab913ee7480166d8f8f4be3fe300
Ganesh Kumar Marrapu <ganeshkumar.marrapu+123@gmail.com>
2023-04-12 14:30:16 +0530

    Little changes are made

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -1,9 +1,15 @@
-﻿namespace FirstBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlazorApp.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(12 , ErrorMessage ="Grocery name should be less than 12 characters.")]
         public string Name { get; set; }
 
+        [Required]
+        [Range(1,60000,ErrorMessage ="Valid price range is (1-60000)")]
         public float Price { get; set; }
 
     }
